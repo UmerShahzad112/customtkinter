43fc4a857f24a54b493eb4049fde59ed0e2e03c0
UmerShahzad112 <mrumershahzad113@gmail.com>
2024-02-08 14:46:02 +0500

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modified_customtkiter',
+    version='1.0.0',
+    description='A modified version of customtkinter',
+    author='Umer Shahzad',
+    author_email='mrumershahzad113@gmail.com',
+    url='https://github.com/UmerShahzad112/customtkinter',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
